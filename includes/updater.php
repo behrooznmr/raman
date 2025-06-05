dd3ca01eadfc72a85674d43c9f3ccc30c14f0297
behrooznmr <behrooznm5@gmail.com>
2025-06-05 12:58:20 +0330

    add services add woocommerce checker

---
+++ includes/updater.php
@@ -0,0 +1,66 @@
+<?php
+defined('ABSPATH') || exit;
+
+add_filter('pre_set_site_transient_update_plugins', 'rcp_check_for_plugin_update');
+add_filter('plugins_api', 'rcp_plugin_update_info', 20, 3);
+
+function rcp_check_for_plugin_update($transient) {
+	if (empty($transient->checked)) return $transient;
+
+	$plugin_slug = plugin_basename(__FILE__);
+	$plugin_slug = 'raman-customize-plugin/raman-customize-plugin.php';
+	$current_version = $transient->checked[$plugin_slug];
+
+	$remote = wp_remote_get('https://ramanagency.ir/raman-customize-plugin-update.json');
+
+	if (is_wp_error($remote)) return $transient;
+
+	$data = json_decode(wp_remote_retrieve_body($remote));
+
+	if (
+		isset($data->version) &&
+		version_compare($data->version, $current_version, '>')
+	) {
+		$transient->response[$plugin_slug] = (object)[
+			'slug'        => 'raman-customize-plugin',
+			'plugin'      => $plugin_slug,
+			'new_version' => $data->version,
+			'tested'      => $data->tested ?? '6.5',
+			'requires'    => $data->requires ?? '5.0',
+			'package'     => $data->download_url,
+		];
+	}
+
+	return $transient;
+}
+
+function rcp_plugin_update_info($res, $action, $args) {
+	if ($action !== 'plugin_information') return $res;
+
+	if ($args->slug !== 'raman-customize-plugin') return $res;
+
+	$remote = wp_remote_get('https://ramanagency.ir/raman-customize-plugin-update.json');
+
+	if (is_wp_error($remote)) return $res;
+
+	$data = json_decode(wp_remote_retrieve_body($remote));
+
+	$res = (object)[
+		'name'          => 'Raman Customize Plugin',
+		'slug'          => 'raman-customize-plugin',
+		'version'       => $data->version,
+		'author'        => '<a href="https://raman.agency">Behrooz Nematmorad</a>',
+		'author_profile'=> 'https://raman.agency',
+		'homepage'      => 'https://raman.agency',
+		'download_link' => $data->download_url,
+		'requires'      => $data->requires ?? '5.0',
+		'tested'        => $data->tested ?? '6.5',
+		'sections'      => [
+			'description' => $data->sections->description ?? '',
+			'changelog'   => $data->sections->changelog ?? '',
+		],
+	];
+
+	return $res;
+}
+
