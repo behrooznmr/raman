c129c31c9073a40b6eac32dbed7bcf89210529fb
behrooznmr <behrooznm5@gmail.com>
2025-06-21 17:04:44 +0330

    start html

---
+++ home-page.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Template Name: Home page
+ *
+ * @package WordPress
+ * @subpackage Raman Theme
+ * @since Raman Theme v1.0
+ */
+
+wp_head();
+?>
+    <div class="raman-home-page container-fluid p-0">
+        <div class="main-wrapper d-flex flex-column" style="height: 1300px;background-color:#333;">
+            <div class="raman-container flex-grow-1 py-5">
+                <!-- hero section - section1-->
+                <div class="ra-hero-section row">
+                    <!--hero section-->
+                    <div class="col-12">
+                        Hero Section
+                        <div class="et_pb_button_module_wrapper et_pb_button_0_wrapper et_pb_button_alignment_center et_pb_module white_light_border et_had_animation" style="">
+                            <a class="et_pb_button et_pb_button_0 white_light_border_inner et_pb_bg_layout_light" href="#services">Voir nos services</a>
+                        </div>
+
+                    </div>
+                    <!--Logo section-->
+                    <div class="col-12">
+                        Logo Section
+                    </div>
+                </div>
+                <!--section2-->
+                <div class="ra-serv-sec row">
+                    <!--Services-->
+                    <div class="col-12">
+                        Hero Section
+                    </div>
+                    <!--about-->
+                    <div class="col-12">
+                        About Section
+                    </div>
+                    <div class="col-12">
+                        <div class="row">
+                            <!--Step-->
+                            <div class="col-12 col-md-6">
+                                Step Box
+                            </div>
+                            <!--Tools-->
+                            <div class="col-12 col-md-6">
+                                Web tools Box
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--section3-->
+                <div class="ra-project-sec row">
+                    <div class="col-12">
+                        Portfolio Section
+                    </div>
+                    <div class="col-12">
+                        Logo Portfolio
+                    </div>
+                </div>
+                <!--section4-->
+                <div class="ra-records-sec row">
+                    <div class="col-12">
+                        data report
+                    </div>
+                    <div class="col-12">
+                        comment customers
+                    </div>
+                </div>
+                <!--section5-->
+                <div class="ra-con-blog-sec row">
+                    <div class="col-12">
+                      Form consulting
+                    </div>
+                    <div class="col-12">
+                       blog
+                    </div>
+                </div>
+            </div>
+
+            <footer class="footer">
+            </footer>
+        </div>
+    </div>
+
+
+<?php
