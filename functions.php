ba08a9ec07eb8fafc75da52a56989fa048eb8ffd
behrooznmr <behrooznm5@gmail.com>
2025-06-24 17:06:26 +0330

    dev

--- functions.php
+++ functions.php
@@ -40,6 +40,11 @@ function raman_theme_scripts() {
 	wp_enqueue_style('bootstrap', trailingslashit( RA_ASSETS ) . 'css/bootstrap.rtl.min.css', array());
 	wp_enqueue_script('raman-bootstrap', trailingslashit( RA_ASSETS ) . 'js/bootstrap.bundle.min.js', array('jquery') , true);
 
+	//swiper
+	wp_enqueue_style('swiper', trailingslashit( RA_ASSETS ) . 'css/swiper-bundle.min.css', array());
+	wp_enqueue_script('js-swiper', trailingslashit( RA_ASSETS ) . 'js/swiper-bundle.min.js', array('jquery') , true);
+
+
 	// front scripts
     wp_enqueue_style('raman-front', trailingslashit( RA_ASSETS ) . 'css/front.css', array(), RA_VER);
 	wp_enqueue_script('raman-front', trailingslashit( RA_ASSETS ) . 'js/front.js', array('jquery'), RA_VER , true);
--- home-page.php
+++ home-page.php
@@ -11,7 +11,7 @@ wp_head();
 ?>
     <div class="raman-home-page container-fluid p-0">
         <div class="main-wrapper d-flex flex-column" style="height: 1300px;">
-            <div class="raman-container flex-grow-1 p-0">
+            <div style="background-color:#030303" class="raman-container flex-grow-1 p-0">
                 <!-- hero section - section1-->
                 <div class="ra-hero-section row p-0">
                     <!--hero section-->
@@ -19,33 +19,95 @@ wp_head();
                         <div class="min-vh-100 hero-section-bc-img">
                             <div class="min-vh-100 w-100 hero-section-overlay">
                                 <h1>
-                                    <div>آژانــس خلاقــیت</div>
-                                    <div>رامـــــــــــــان</div>
+                                    آژانــس خلاقــیت <span>رامـــان</span>
                                 </h1>
-                                <h1>
-                                    <span>RAMAN</span>
-                                    <br>
-                                    <span>CREATIVE AGENCY</span>
-                                </h1>
-
-
+                                <div class="en-title animated-text">
+                                    <span>R</span><span>A</span><span>M</span><span>A</span><span>N</span>
+                                    <span class="space"> </span>
+                                    <span>C</span><span>R</span><span>E</span><span>A</span><span>T</span><span>I</span><span>V</span><span>E</span>
+                                    <span class="space"> </span>
+                                    <span>A</span><span>G</span><span>E</span><span>N</span><span>C</span><span>Y</span>
+                                </div>
+                                <p>ارائه دهنده خدمات طراحی سایت، سئو و بهینه سازی سایت، طراحی لندینگ پیج، افزایش امنیت
+                                    سایت، افزایش سرعت سایت و مشاوره کسب و کار</p>
                                 <div class="glow-button-wrapper">
                                     <a href="#" class="glow-button">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#010101" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
                                         دریافت مشاوره رایگان
                                         <span class="animated-border-box-glow"></span>
                                         <span class="animated-border-box"></span>
                                     </a>
                                 </div>
+                            </div>
+                            <div class="logo-carousel-wrapper col-12">
+                                <div class="logo-carousel">
+                                    <div class="carousel-track">
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/1.png"
+                                                    alt="Logo 1"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/2.png"
+                                                    alt="Logo 2"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/3.png"
+                                                    alt="Logo 3"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/4.png"
+                                                    alt="Logo 4"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/5.png"
+                                                    alt="Logo 5"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/6.png"
+                                                    alt="Logo 6"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/7.png"
+                                                    alt="Logo 6"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/8.png"
+                                                    alt="Logo 6"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/9.png"
+                                                    alt="Logo 6"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/10.png"
+                                                    alt="Logo 6"></div>
 
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/1.png"
+                                                    alt="Logo 1"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/2.png"
+                                                    alt="Logo 2"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/3.png"
+                                                    alt="Logo 3"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/4.png"
+                                                    alt="Logo 4"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/5.png"
+                                                    alt="Logo 5"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/6.png"
+                                                    alt="Logo 6"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/7.png"
+                                                    alt="Logo 6"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/8.png"
+                                                    alt="Logo 6"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/9.png"
+                                                    alt="Logo 6"></div>
+                                        <div class="ra-logo-carousel-item"><img
+                                                    src="<?php echo get_template_directory_uri(); ?>/assets/images/10.png"
+                                                    alt="Logo 6"></div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                     <!--   <div class="et_pb_button_module_wrapper et_pb_button_0_wrapper et_pb_button_alignment_center et_pb_module white_light_border et_had_animation" style="">
-                            <a class="et_pb_button et_pb_button_0 white_light_border_inner et_pb_bg_layout_light" href="#services">Voir nos services</a>
-                        </div>-->
-                    </div>
-                    <!--Logo section-->
-                    <div class="col-12">
-                        Logo Section
+
                     </div>
                 </div>
                 <!--section2-->
@@ -92,10 +154,10 @@ wp_head();
                 <!--section5-->
                 <div class="ra-con-blog-sec row">
                     <div class="col-12">
-                      Form consulting
+                        Form consulting
                     </div>
                     <div class="col-12">
-                       blog
+                        blog
                     </div>
                 </div>
             </div>
